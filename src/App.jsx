5d9370c6bd9325c565158db3c9b324216fea7005
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-12 15:54:33 +0530

    Login Page has been made

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 
 
 function App(){
   return(
     <div>
-    <Home />
+    <Routes>
+      <Route path= "/" element={<Home />}/>
+      <Route path= "/login" element={<Login />}/>
+    </Routes>
     </div>
   )
 }
-
 
 export default App;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,33 @@
 import './Login.css'
+import logo from '../../assets/logo.png'
 
-function Login(){
-  return(
+function Login() {
+  return (
     <div className="login">
+      <img src={logo} className='login-logo' />
+      <div className="login-form">
+        <h1>Sign Up</h1>
+        <form action="">
 
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='email' />
+          <input type="password" placeholder='Enter a password' />
+          <button>Sign Up</button>
+
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember me</label>
+            </div>
+            <p>Need help?</p>
+          </div>
+
+        </form>
+        <div className="form-switch">
+          <p>New to Netflix?<span>Sign Up Now</span></p>
+          <p>Already have account?<span>Sign In Now</span></p>
+        </div>
+      </div>
     </div>
   )
 }
