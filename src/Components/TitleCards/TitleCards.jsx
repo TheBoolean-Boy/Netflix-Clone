6123fcaeec368658e8c048f4657f2c55b1531809
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-12 13:03:10 +0530

    UI part done

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -1,10 +1,35 @@
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data.js'
+import { useEffect, useRef } from 'react'
 
 
-function TitleCards(){
-  return(
-    <div className="titlecards">
 
+function TitleCards({title, category}) {
+  const cardRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    const delta = event.deltaY !== 0 ? event.deltaY : event.deltaX;
+    cardRef.current.scrollLeft += delta;
+  }
+
+  useEffect(() => {
+    cardRef.current.addEventListener('wheel', handleWheel, {passive: false})
+  }, [])
+
+  return (
+    <div className="title-cards">
+      <h2>{title? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardRef}>
+        {
+          cards_data.map((card, index) => {
+            return <div key={index} className="card">
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          })
+        }
+      </div>
     </div>
   )
 }
